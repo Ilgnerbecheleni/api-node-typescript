6d340c0fcfbdb9b2af22d4c94dfadda426d189ce
ilgner becheleni <ilgner.gui@gmail.com>
2023-05-13 23:16:12 -0300

    feat: config editor de codigo

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
 
+interface user {
+    // eslint-disable-next-line linebreak-style
+
+}
 const server = express();
-
 server.get('/', (_: Request, res: Response) => {
-    res.send({ api: "API V 1.0.0" })
-})
-
+    res.send({ api: 'API V 1.0.0' });
+});
 
 export { server };
