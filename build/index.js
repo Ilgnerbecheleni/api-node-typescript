e25086072c647216b5611a995dd49d7202227335
Ilgner Aparecido Becheleni <81272773+Ilgnerbecheleni@users.noreply.github.com>
2023-05-14 01:06:51 -0300

    Delete build directory

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server/server");
-const port = process.env.PORT || 3000;
-server_1.server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
